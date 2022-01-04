f294b20a1b4802648c008bb1179558300ca0f3de
MarquisPierre <marquispierre27@gmail.com>
2022-01-04 04:03:35 -0500

    updated router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AddPost from "./components/AddContact";
 import EditContact from "./components/EditContact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import "./styles.css";
+import { HashRouter as Router } from 'react-router-dom'
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <ToastContainer />
       <Navbar />
+      <Router basename={process.env.PUBLIC_URL}>
       <Route exact path="/" component={() => <Home />} />
       <Route exact path="/add" component={() => <AddPost />} />
       <Route exact path="/edit/:id" component={() => <EditContact />} />
+      </Router>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import {HashRouter} from "react-router-dom"
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "react-toastify/dist/ReactToastify.css";
 
 import { createStore } from "redux";
@@ -17,9 +15,9 @@ const store = createStore(contactReducer, composeWithDevTools());
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <HashRouter>
+    <App />
+  </HashRouter>
   </Provider>,
   rootElement
 );
